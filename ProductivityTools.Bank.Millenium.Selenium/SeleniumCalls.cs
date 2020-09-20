1ee293b96e70a8a097eeba552e29c8be2a229278
Pawel Wujczyk <pwujczyk@hotmail.com>
2020-09-20 23:08:03 +0200

    login

--- ProductivityTools.Bank.Millenium.Selenium/SeleniumCalls.cs
+++ ProductivityTools.Bank.Millenium.Selenium/SeleniumCalls.cs
@@ -2,6 +2,7 @@
 using OpenQA.Selenium.Chrome;
 using System;
 using System.Net.Sockets;
+using System.Threading;
 
 namespace ProductivityTools.Bank.Millenium.Selenium
 {
@@ -21,6 +22,20 @@ namespace ProductivityTools.Bank.Millenium.Selenium
         public void Login(string login, string password)
         {
             this.Chrome.Url = Addresses.LoginPage;
+
+            var loginBox=this.Chrome.FindElement(By.ClassName("field_row-client_id_field"));
+            var loginControl = loginBox.FindElement(By.TagName("input"));
+            Thread.Sleep(2000);
+            loginControl.SendKeys(login);
+            var dalejControl = this.Chrome.FindElement(By.ClassName("button-dalej"));
+            dalejControl.Click();
+
+            Thread.Sleep(2000);
+            var passwordBox = this.Chrome.FindElement(By.ClassName("field_row-password_field"));
+            var passwordControl = passwordBox.FindElement(By.TagName("input"));
+            passwordControl.SendKeys(password);
+            var loginButtonControl = this.Chrome.FindElement(By.ClassName("button-login"));
+            loginButtonControl.Click();
         }
     }
 }
