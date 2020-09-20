3c1e7083b56fb263021f49749466d711e08e55e1
Pawel Wujczyk <pwujczyk@hotmail.com>
2020-09-20 22:15:34 +0200

    login and password

--- ProductivityTools.Bank.Inteligo/Program.cs
+++ ProductivityTools.Bank.Inteligo/Program.cs
@@ -1,6 +1,8 @@
-﻿using Microsoft.Extensions.DependencyInjection;
+﻿using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
 using ProductivityTools.Bank.Millenium.App.Runner;
 using ProductivityTools.Bank.Millenium.Selenium;
+using ProductivityTools.MasterConfiguration;
 using System;
 using System.Security.Authentication.ExtendedProtection;
 
@@ -12,12 +14,25 @@ namespace ProductivityTools.Bank.Inteligo.Runner
         {
             Console.WriteLine("Hello World!");
 
+            string environment = Environment.GetEnvironmentVariable("ASPNETCORE_ENVIRONMENT");
+
+            if (String.IsNullOrWhiteSpace(environment))
+                throw new ArgumentNullException("Environment not found in ASPNETCORE_ENVIRONMENT");
+
+            IConfigurationRoot configuration = new ConfigurationBuilder()
+                .AddMasterConfiguration()
+                .Build();
+
+            var login = configuration["login"];
+            var password = configuration["password"];
+
             var serviceProvider = new ServiceCollection()
                 .AddSingleton<InteligoApplication>()
                 .AddSingleton<SeleniumCalls>()
                 .BuildServiceProvider();
             var application = serviceProvider.GetService<InteligoApplication>();
-            application.Run("fdsa", "fdsa");
+
+            application.Run(login, password);
             Console.ReadLine();
         }
     }
