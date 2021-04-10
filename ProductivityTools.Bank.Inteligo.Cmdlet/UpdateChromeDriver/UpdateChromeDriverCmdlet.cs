01987b5ca448f1079c99341150177270c5913c73
Pawel Wujczyk <pwujczyk@hotmail.com>
2021-04-10 21:43:23 +0200

    update chrome driver

---
+++ ProductivityTools.Bank.Inteligo.Cmdlet/UpdateChromeDriver/UpdateChromeDriverCmdlet.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Management.Automation;
+using System.Text;
+
+namespace ProductivityTools.Bank.Inteligo.Cmdlet.UpdateChromeDriver
+{
+    [Cmdlet("Update", "InteligoChromeDriver")]
+    public class UpdateChromeDriverCmdlet : PSCmdlet.PSCmdletPT
+    {
+        [Parameter]
+        public SwitchParameter ShowBrowser { get; set; }
+
+
+        public UpdateChromeDriverCmdlet()
+        {
+        }
+
+        protected override void ProcessRecord()
+        {
+            GetChromeDriver.ChromeDriver.DownloadLatestVersion();
+            base.ProcessRecord();
+        }
+    }
+}
